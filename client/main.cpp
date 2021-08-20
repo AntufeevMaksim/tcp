be252a2fe03f0609511cdb933b85419d6f318b07
AntufeevMaksim <87116331+AntufeevMaksim@users.noreply.github.com>
2021-08-20 11:10:51 +0700

    Update main.cpp
    communication client-server

--- client/main.cpp
+++ client/main.cpp
@@ -3,7 +3,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdio.h>
-
+#include <iostream>
 
 
 
@@ -11,7 +11,7 @@ int main(){
   struct sockaddr_in local;
   int s;
   int rc;
-  char buf [2];
+  char buf [10];
 
   local.sin_family = AF_INET;
   local.sin_port = htons(8888);
@@ -28,21 +28,26 @@ int main(){
     perror("error connect");
     return 1;
   }
-  
-  rc = send(s, "1", 1, 0);
-  if (rc <= 0){
-    perror("error send");
-    return 1;
-  }   
+  while (true){
+    std::string input;
+    std::cin >> input;
+    rc = send(s, input.c_str(), 10, 0);
+    if (rc <= 0){
+      perror("error send");
+      return 1;
+    }   
 
-  rc = recv(s, buf, 2, 0);
-  if (rc <= 0){
-    perror("error recv");
-    return 1;
-  } 
-  else{
-    printf("%c", buf[0]);
-    printf("%c\n", buf[1]);
+    rc = recv(s, buf, 10, 0);
+    if (rc <= 0){
+      perror("error recv");
+      return 1;
+    } 
+    else{
+      for (char c : buf){
+      printf("%c", c);
+      }
+      printf("\n");
+    }
   }
   //shutdown(listner, SHUT_RDWR);
   return 0;
