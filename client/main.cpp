34a1f5f225bc5634c949a646ceb2aace9067d2fd
AntufeevMaksim <87116331+AntufeevMaksim@users.noreply.github.com>
2021-08-28 13:06:18 +0700

    Update main.cpp
    Messenger ready.
    1. Run server.
    2. Run two clients.
    3. Start write a message and press "Enter" to send.

--- client/main.cpp
+++ client/main.cpp
@@ -9,28 +9,65 @@
 #include <chrono>
 #include <poll.h>
 #include <vector>
+#include <sys/ioctl.h>
 
 std::vector<char> Read(int s, fd_set readfs, timeval tv){
   std::vector<char> buf(10);
+  int n;
+  unsigned int m;
 
-  //printf("a");
 
   int res = select(s+1, &readfs, NULL, NULL, &tv);
   if (res>0){
-    int rc = recv(s, buf.data(), 1, 0);
+    ioctl(s, TIOCINQ, &n);
+    int rc = recv(s, buf.data(), n, 0);
     return (rc == 0 ? std::vector<char>{} : buf);
   }
   else{
     return {};
-    //return -1;
   }
 }
 
-void PrintAnswer(/*char buf*/ const std::vector<char>& buf){
-//  printf("%c\n", buf);
+
+// void ChangeColor(int color){
+//   init_pair(1, color, COLOR_BLACK);
+//   attron(COLOR_PAIR(1));
+// }
+
+void PrintAnswer(const std::vector<char>& buf){
+//  ChangeColor(COLOR_BLUE);
+//  printw("\033[1;31m");
   printw("%s\n", buf.data());
-//  printf("%s\n", buf.data());
+//  refresh();
+//  ChangeColor(COLOR_WHITE);
 }
+
+void Input(std::vector<char>& input, WINDOW* w){
+  char sim = getch();
+  if (sim == ERR){
+    return;
+  }
+
+  input.push_back(sim);
+  nodelay(w, FALSE);
+  while (true){
+    char sim = getch();
+    if (sim != '\n'){
+      input.push_back(sim);
+    }
+    else{
+      break;
+    }
+  }
+  nodelay(w, TRUE);
+  int y = getcury(w);
+  wmove(w, y+1, 0);
+//  refresh();
+  //printw("\n"); 
+}
+
+
+
 
 
 int main(){
@@ -40,18 +77,10 @@ int main(){
   nodelay(w, TRUE);
 	raw();				/* Line buffering disabled	*/
 	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
-	// noecho();	
-  // curs_set(0);
-
-  // int dir = getch();
-  // if (dir == ERR){
-  //   dir = 258;
-  // }
-
+//  start_color();
   struct sockaddr_in local;
   int s;
   int rc;
-//  char buf;
 
   local.sin_family = AF_INET;
   local.sin_port = htons(8888);
@@ -79,27 +108,33 @@ int main(){
 
 
 
+  std::vector<char> input;
   while (true){
 
     auto buf = Read(s, readfs, tv);
-    if (/*buf!=-1*/ !buf.empty()){
+    if (!buf.empty()){
       PrintAnswer(buf);
     }
 
 
     std::this_thread::sleep_for(10ms);
-    char dir = getch();
-    if (dir == 's'){
-      break;
-    }
-    if (dir != ERR){
-      rc = send(s, &dir, 1, 0);
+
+    // char dir = getch();
+    // if (dir == 's'){
+    //   break;
+    // }
+    Input(input, w);
+
+
+
+    if (!input.empty()){
+      rc = send(s, input.data(), input.size(), 0);
+      input.clear();
       if (rc <= 0){
         perror("error send");
         return 1;
       }   
     }
   }
-  //shutdown(listner, SHUT_RDWR);
   return 0;
 }
